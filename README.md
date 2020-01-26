b2b220ce2fde83daa41edb0b508b4b5cff6bf64e
anilchapagain68 <anilchapagain68@gmail.com>
2020-01-26 17:16:00 +0530

    connected mongolab

---
+++ README.md
@@ -0,0 +1,12 @@
+# SimpleBLogCMS
+############ Tools || Framework || Module used in project 
+Node.js
+MongoDB
+Angular 8
+Angular CLI
+Express.js
+Passport.js
+Mongoose.js
+CKEditor 4
+Terminal or Command Line
+IDE or Text Editor
---
+++ client/src/app/bycategory/bycategory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bycategory',
+  templateUrl: './bycategory.component.html',
+  styleUrls: ['./bycategory.component.css']
+})
+export class BycategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
